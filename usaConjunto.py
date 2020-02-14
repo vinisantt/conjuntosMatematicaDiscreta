d06dbd8cac890a11a6ec7ad96915a5c585a8d5a0
Jonhtan Mota Dos Reis <Jmtrs@rede.ulbra.br>
2020-02-14 20:26:12 -0300

    pulled

--- usaConjunto.py
+++ usaConjunto.py
@@ -2,11 +2,12 @@ import Conjuntos
 
 Conjunto = Conjuntos.Conjunto
 
-conjunto_numeros1 = Conjunto("B", 6, 8, 1)
+conjunto_numeros1 = Conjunto("B", 1, 2, 3)
 conjunto_numeros = Conjunto("B", 1, 2, 3)
 conjunto_letras = Conjunto("C", 'd', "e", "g")
 conjunto_mesclado = Conjunto(
     "A", conjunto_numeros, "a", "c", conjunto_letras)
 
 conjunto_mesclado.imprimir()
-print(conjunto_mesclado.pertence(2))
+#print(conjunto_mesclado.pertence(2))
+print(conjunto_numeros1.contem(conjunto_numeros))
